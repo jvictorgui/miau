8d594f7d2feefdab5a4d23599684dddeab6df290
Joao Victor <druvao136@gmail.com>
2022-10-24 20:48:29 -0300

    a

---
+++ fundamentos/4-introdução-a-JavaScript/dia1/script.js
@@ -0,0 +1,8 @@
+const myName = 'joao victor' 
+const birthCity =  'serrolandoa'
+birthYear = 2030
+
+console.log (myName + '' +birthCity + ' ' + birthYear)
+
+const birthCity = jacobina
+console.log ( birthCity)
