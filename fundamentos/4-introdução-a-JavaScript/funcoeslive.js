4235100599fd040cef0f0b55354c01722ca4aee4
Joao Victor <druvao136@gmail.com>
2022-10-25 18:11:39 -0300

    atvs

---
+++ fundamentos/4-introdução-a-JavaScript/funcoeslive.js
@@ -0,0 +1,14 @@
+// function saudacoes (nome, sobrenome){
+//     return 'hello ' + nome + ' ' + sobrenome
+// } 
+// console.log(saudacoes('joao', 'guimaraes'))
+// console.log(saudacoes('marcos', 'guilherme'))
+// console.log(saudacoes('luan', 'lorenço'))
+
+function resultado(vitoria, empate){
+    let pontoInicial = 0
+
+    return pontoInicial = vitoria *3 + empate *1
+}
+console.log(resultado(14,24))
+
---
+++ fundamentos/4-introdução-a-JavaScript/script.js
@@ -0,0 +1,27 @@
+
+
+//   let jogador = {
+//     nome: 'Cristiano Ronald',
+//     Clube: 'Manchester United',
+//     filhos: ['jorgina', 'Florentina', 'Ronaldo Jr'],
+//     numeroCamisa: 7,
+//     goles: 700,
+//   }
+//   console.log('o jogador' + jogador.nome)
+
+//   console.log('o jogador' + jogador['nome'])      
+
+//   jogador['nomeEGols'] = jogador.nome + ' ' + jogador.goles;
+//   console.table(jogador)
+
+
+let livro ={
+    paginas: 350,
+    tema: 'fiction',
+    autor: 'george martin',
+    mainChar: ['obama', 'naldo', 'george'],
+
+
+} 
+console.log (livro.tema + livro.mainChar[1])
+
