c78d1b3f57da80bf8e33787a67bac8a34cbbb60b
Joao Victor <druvao136@gmail.com>
2022-10-26 19:01:57 -0300

    atvs dia 5

---
+++ fundamentos/4-introdução-a-JavaScript/dia 5/script.js
@@ -0,0 +1,37 @@
+ 
+
+// //  function contaIdade (){
+// //     for(let contador = 0; contador <=80; contador++){
+// //         console.log(contador)
+// //     }
+// //  }  
+// //  contaIdade();
+    
+//     // Com o template literals
+// console.log(`Primeira linha;
+// Segunda linha;
+// Terceira linha;`
+// );
+
+// // Sem o template literals:
+// console.log('Primeira linha;\n' + 'Segunda linha;\n' + 'Terceira linha;\n');
+
+// const nome = {
+//     user: 'joao',
+//     idade:23,
+// }
+
+
+// const nome.idade = 24
+// console.log(nome)
+
+const nome = 'joao'
+ console.log(`ola ${nome} !`)
+
+ const soma = (n1,n2) =>  n1 +n2
+    
+ 
+ console.log(soma(12,34))
+
+ console.log(`some de 12 e 34 = ${soma(12,34)}`)
+
