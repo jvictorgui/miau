5020db5523b6fa1ac9429e6c09bded5c23933d8b
Joao Victor <druvao136@gmail.com>
2022-10-27 06:31:58 -0300

    add pasta 2

---
+++ fundamentos/4-introdução-a-JavaScript/dia2/script.js
@@ -0,0 +1,12 @@
+pizzas =['frang0','quijo','calabresa',]
+console.log(pizzas);
+
+pizzas[3] = 'peito de peru'
+console.log(pizzas);
+pizzas.push('sertaneja');
+console.log(pizzas.sort());
+pizzas.push('gratinada')
+
+for(let contador =0; contador < pizzas.length; contador++){
+    console.log(pizzas[contador])
+}
